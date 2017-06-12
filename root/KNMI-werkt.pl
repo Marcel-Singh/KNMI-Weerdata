e48d2dc029abbd661626fa4a286f74e5d5a03ccc
Marcel-Singh <15099229@student.hhs.nl>
2017-06-12 13:14:44 +0200

    KNMI-werkt

--- root/KNMI-werkt.pl
+++ root/KNMI-werkt.pl
@@ -1,37 +1,54 @@
 use strict;
 use warnings;
 use POSIX 'strftime';
-
+use CGI qw(:standard); ##query
 
 use LWP::UserAgent;
- 
 my $ua = LWP::UserAgent->new;
- 
+
+#######Query#######
+my $query = new CGI;
+my $datum = $query->param('datum');
+
 my $server_endpoint = "http://projects.knmi.nl/klimatologie/daggegevens/getdata_dag.cgi";
  
 # set custom HTTP request header fields
 my $req = HTTP::Request->new(POST => $server_endpoint);
  
  my $STATIONS = "330:344";
- my $OPTIES = "TG:TN:TX";
+ my $OPTIES = "TG";
+
  my $date = strftime '%Y%m%d', localtime;
- my $VAN = 20161111;
- my $TOT = $date -1;
+ my $DM = strftime '%m%d', localtime;
+
+ my $FYEAR = "1980"; #1-1-1971
+ my $FMONTH = "02";
+ my $FDAY   = "22";
+
+ my $TYEAR = "1980";
+ my $TMONTH = "02";
+ my $TDAY   = "22";
+
+
+ my $VAN = $FYEAR . $FMONTH . $FDAY;
+ my $TOT = $TYEAR . $TMONTH . $TDAY;
+
 
  #TG = Gemiddelde TEMP
  #TN = Min TEMP
  #TX = Max TEMP
 
 # add POST data to HTTP request body
-my $post_data = "stns=$STATIONS&vars=$OPTIES&start=$VAN&end=$TOT";
+my $post_data = "stns=$STATIONS&vars=$OPTIES&start=$datum&end=$datum";
 $req->content($post_data);
  
 my $resp = $ua->request($req);
+
 if ($resp->is_success) {
 
     my $message = $resp->decoded_content;
 
-#verwijder comments
+    #verwijder comments
  	while ($message=~ m/^#.*\n/) {
 $message=~ s/^#.*\n//g;
 };
@@ -39,21 +56,37 @@ $message=~ s/^#.*\n//g;
 #preppen
  	while ($message=~ m/ /) {
 $message=~ s/ //g;
-$message=~ s/330,//g;
-$message=~ s/344,//g;
+$message=~ s/330,.........//g;
+$message=~ s/344,.........//g;
 };
 
-	print $message;
+   my @lines= split /\n/, $message;
 
-   my @temp= split /,/, $message;
+my $aap;
+my $file;
+foreach $aap (@lines) {
+$aap = $aap * 0.1;
 
-
-   open(my $file, '>', 'weerdata.txt');
-   print $file $message;
-   close $file;
-
-#print $message->content;
 }
+
+sub average {
+my @array = @_; # save the array passed to this function
+my $sum; # create a variable to hold the sum of the array's values
+foreach (@array) { $sum += $_; } # add each element of the array 
+# to the sum
+return $sum/@array; # divide sum by the number of elements in the
+# array to find the mean
+}
+
+my $GemiddeldeTemp = average(@lines);
+#print $avgOfArray;
+# here the average will be 5
+
+open($file, '>>', "weerdata.txt");
+print $file $datum . "," .$GemiddeldeTemp . "\n";
+close $file;
+
+ }
 else {
     print "HTTP POST error code: ", $resp->code, "\n";
     print "HTTP POST error message: ", $resp->message, "\n";
